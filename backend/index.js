1f8012ed2ad812ae6bd18df0f7420d3d34953844
caoyu15 <caoyu15@baidu.com>
2020-11-18 20:12:43 +0800

    init backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+var session = require("express-session");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
+app.use(session({
+    secret: 'realtime-interview',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        // secure: true, // https
+        maxAge: 60000
+    }
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
