426df31f10445c5a36b4fc95750fbd962516832f
caoyu <ricardocao.biker@gmail.com>
2020-11-23 02:30:31 +0800

    websocket实现

--- frontend/src/components/interview/codeeditor.tsx
+++ frontend/src/components/interview/codeeditor.tsx
@@ -4,6 +4,11 @@ import Button from '@/components/common/button';
 import { Button as AntBtn } from 'antd';
 import { CaretRightFilled } from '@ant-design/icons';
 
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+// @ts-ignore
+import { MonacoBinding } from 'y-monaco'
+
 import './codeeditor.scss';
 
 const CodeEditor: FC = () => {
@@ -16,8 +21,19 @@ const CodeEditor: FC = () => {
       automaticLayout: true
     }
   }, []);
+
+  const ydoc = new Y.Doc()
+  const roomName = 'room1';
+  const provider = new WebsocketProvider('ws://127.0.0.1:1234', roomName, ydoc)
+  const type = ydoc.getText('monaco')
+
+
   const onChange = useCallback((newValue, e) => {
-    console.log('onChange', newValue, e)
+    console.log('onChange')
+  }, []);
+  const editorDidMount = useCallback((editor, monaco) => {
+    const monacoBinding = new MonacoBinding(type, (editor.getModel()), new Set([editor]), provider.awareness);
+    provider.connect();
   }, []);
   return (
     <div className="editor">
@@ -30,6 +46,7 @@ const CodeEditor: FC = () => {
         value={code}
         options={options}
         onChange={onChange}
+        editorDidMount={editorDidMount}
       />
       <div className="bottom-bar">
         <AntBtn className="run-btn" type="primary" icon={< CaretRightFilled />} color="red">运行</AntBtn>
