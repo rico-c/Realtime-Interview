1ea990f8ac0a84cbf4602cbdde45c53439d2d022
rico <ricardocao.biker@gmail.com>
2023-09-07 19:39:14 +0800

    css

--- frontend/src/components/homepage/footer.tsx
+++ frontend/src/components/homepage/footer.tsx
@@ -1,11 +1,25 @@
-import './footer.scss';
-import React from 'react';
+import "./footer.scss";
+import React from "react";
+import languageList from "utils/Languages";
 
 export const Footer = () => {
   return (
     <div className="footer">
-      <div>联系方式：ricardo_cao@foxmail.com</div>
-      <a href="https://beian.miit.gov.cn/" target="blank">京ICP备18027318号-3</a>
+      <div className="w-1/2 pb-2 border-b border-gray">
+        <div className="font-bold text-lg pb-2 text-white">支持语言</div>
+        <div className="flex flex-wrap gap-1">
+          {languageList.map((lang) => (
+            <div className="mr-5 text-sm text-grey">{lang.name}</div>
+          ))}
+        </div>
+      </div>
+      <div className="w-1/2 py-2 border-b border-gray">
+        <div className="font-bold text-lg pb-2 text-white">联系方式</div>
+        ricardocao.biker@gmail.com
+      </div>
+      <div className="w-1/2 py-2 text-sm">
+        COPYRIGHT © 2019-2023 深圳市氪林科技有限公司
+      </div>
     </div>
-  )
-}
+  );
+};
--- frontend/src/components/homepage/homepage.tsx
+++ frontend/src/components/homepage/homepage.tsx
@@ -1,63 +1,69 @@
 import React, { FC, useCallback } from "react";
-import { Button, message } from 'antd';
+import { Button, message } from "antd";
 import { useHistory } from "react-router-dom";
 import { createInterview, createRoomid } from "actions";
-import Intro from 'assets/imgs/homepageIntro.png';
-import './homepage.scss';
+import Intro from "assets/imgs/homepageIntro.png";
+import "./homepage.scss";
 
 const HomePage: FC = () => {
   const history = useHistory();
 
   const jumpDemo = useCallback(async () => {
-    const roomid = await createRoomid('trydemo');
+    const roomid = await createRoomid("trydemo");
     if (!roomid) {
-      message.error('创建面试ID失败');
+      message.error("创建面试ID失败");
     }
     const res = await createInterview({
       id: roomid,
-      creator: 'trydemo',
-      teamId: 'trydemo',
-      type: 2
+      creator: "trydemo",
+      teamId: "trydemo",
+      type: 2,
     });
     if (res.code === 0) {
       history.push({
-        pathname: `/interview/${roomid}`, state: {
-          demo: true
-        }
-      })
-    }
-    else {
-      message.error('创建失败，请稍后再试');
+        pathname: `/interview/${roomid}`,
+        state: {
+          demo: true,
+        },
+      });
+    } else {
+      message.error("创建失败，请稍后再试");
     }
   }, []);
 
   return (
     <div className="home-page">
-      <div className="title">一站式在线技术面试平台</div>
-      <div className="main">
-        <div className="left">
+      <div className="title text-center">一站式在线技术面试平台</div>
+      <div className="main flex justify-center">
+        <div className="left text-center">
           <div className="sub-title">多人同步的在线VSCode编辑器</div>
           <div className="sub-title">17种主流代码语言实时编译</div>
           <div className="sub-title">高清视频语音通话</div>
-          <Button className="try" type="primary" onClick={jumpDemo}>试用</Button>
+          <Button className="try my-10" type="primary" onClick={jumpDemo}>
+            立即试用
+          </Button>
         </div>
-        <div className="right">
-          <img src={Intro} alt="realtime-interview" />
-        </div>
+      </div>
+      <div className="right">
+        <img src={Intro} alt="realtime-interview" />
       </div>
       <div className="desc">
         <div className="desc-item">
           <i className="iconfont">&#xe61a;</i>
           <div>
             <div className="desc-title">VSCode般的顺滑编码体验</div>
-            <div className="desc-txt">多人瞬时同步代码同步，snippets随叫随到</div>
+            <div className="desc-txt">
+              多人瞬时同步代码同步，snippets随叫随到
+            </div>
           </div>
         </div>
         <div className="desc-item">
           <i className="iconfont">&#xe61b;</i>
           <div>
             <div className="desc-title">随意切换主流代码语言</div>
-            <div className="desc-txt">支持Java/Python/PHP/JS/TS/C++/Go等主流语言</div>
+            <div className="desc-txt">
+              支持Java/Python/PHP/JS/TS/C++/Go等主流语言
+            </div>
           </div>
         </div>
         <div className="desc-item">
@@ -69,7 +75,7 @@ const HomePage: FC = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
