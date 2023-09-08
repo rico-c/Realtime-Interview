d251ae9468b36f769f573192b716617eb51a389f
rico <ricardocao.biker@gmail.com>
2023-09-08 12:32:21 +0800

    x

--- backend/index.js
+++ backend/index.js
@@ -15,41 +15,42 @@ const MongoStore = require("connect-mongo")(session);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
-app.use(cors({ credentials: true, origin: ["https://realtime-interview.vercel.app", 'http://localhost:3000'] }));
 app.use(
   session({
     secret: "realtime-interview-secreat",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: true,
       maxAge: 60000 * 60 * 24 * 7,
-      sameSite: 'none'
+      secure: process.env.NODE_ENV === 'development' ? false : true,
+      httpOnly: process.env.NODE_ENV === 'development' ? false : true,
+      sameSite: process.env.NODE_ENV === 'development' ? false : 'none',
     },
     store: new MongoStore({
       url: dbConfig,
       autoRemove: "interval", // 过期自动删除
-      autoRemoveInterval: 24 * 60 * 7 // 7天
+      autoRemoveInterval: 24 * 60 * 7,
     })
   })
 );
+app.enable('trust proxy');
+app.use(cors({credentials: true, origin: ["https://realtime-interview.vercel.app", 'http://localhost:3000']}));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 router(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
